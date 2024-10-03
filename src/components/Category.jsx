48b98e5ebf25ab3c9557c1b7705ff050b1f7d18c
MohamedSbika <sbikamohamed241@gmail.com>
2024-10-03 20:14:28 +0100

    hello 1

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -11,6 +11,7 @@ function Category({ handleChange }) {
           <label className="sidebar-label-container">
             <input onChange={handleChange} type="radio" value="" name="test" />
             <span className="checkmark"></span>All
+            
           </label>
           <Input
             handleChange={handleChange}
